e4f181a5c585ee3841a152a956b6cfa31332ade1
Edson Pereira <ed.1206@gmail.com>
2022-08-02 19:19:45 -0300

    Resolvido requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -11,43 +11,44 @@ import Search from './pages/Search';
 class App extends React.Component {
   render() {
     return (
-      <main>
-        TrybeTunes
-        <Route
-          exact
-          path="/"
-          component={ Login }
-        />
-        <Route
-          exact
-          path="/search"
-          component={ Search }
-        />
-        <Route
-          exact
-          path="/album/:id"
-          component={ Album }
-        />
-        <Route
-          exact
-          path="/favorites"
-          component={ Favorites }
-        />
-        <Route
-          exact
-          path="/profile"
-          component={ Profile }
-        />
-        <Route
-          exact
-          path="/profile/edit"
-          component={ ProfileEdit }
-        />
-        <Route
-          path=""
-          component={ NotFound }
-        />
-      </main>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={ Login }
+          />
+          <Route
+            exact
+            path="/search"
+            component={ Search }
+          />
+          <Route
+            exact
+            path="/album/:id"
+            component={ Album }
+          />
+          <Route
+            exact
+            path="/favorites"
+            component={ Favorites }
+          />
+          <Route
+            exact
+            path="/profile"
+            component={ Profile }
+          />
+          <Route
+            exact
+            path="/profile/edit"
+            component={ ProfileEdit }
+          />
+          <Route
+            path=""
+            component={ NotFound }
+          />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,70 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../Components/Loading';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      disabled: 3,
+      loading: false,
+      goToSearch: false,
+      inputName: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  clickButton = async () => {
+    const { inputName } = this.state;
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name: inputName });
+    this.setState({
+      goToSearch: true,
+      loading: false,
+    });
+  }
+
   render() {
+    const { inputName, disabled, loading, goToSearch } = this.state;
+    if (loading) {
+      return <Loading />;
+    }
+    if (goToSearch) {
+      return <Redirect to="/search" />;
+    }
     return (
       <div
         data-testid="page-login"
       >
-        Login
+        <h1>Login</h1>
+        <form>
+          <input
+            data-testid="login-name-input"
+            placeholder="Nome"
+            type="text"
+            name="inputName"
+            value={ inputName }
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            disabled={ inputName.length < disabled }
+            onClick={ this.clickButton }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
