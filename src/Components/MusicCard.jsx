16b911973e5bfe9b478bdaa1ee69a51b1730651c
Edson Pereira <ed.1206@gmail.com>
2022-08-05 01:36:21 -0300

    Resolvido requisitos 8 e 10

--- src/Components/MusicCard.jsx
+++ src/Components/MusicCard.jsx
@@ -1,15 +1,62 @@
 import { shape, string } from 'prop-types';
 import React, { Component } from 'react';
+import { addSong, removeSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      favsList: [],
+      isFavorite: false,
+    };
+  }
+
+  async componentDidMount() {
+    this.getFavsList();
+  }
+
+  handleSave = async ({ target: { checked } }) => {
+    const { musicInfo } = this.props;
+    this.setState({
+      loading: true,
+      isFavorite: checked,
+    });
+    if (checked) {
+      await addSong(musicInfo);
+    } else {
+      await removeSong(musicInfo);
+    }
+    this.setState({
+      loading: false,
+    });
+  };
+
+  getFavsList = async () => {
+    const { trackId } = this.props;
+    const saveFavs = await getFavoriteSongs();
+    this.setState({ favsList: saveFavs });
+    const { favsList } = this.state;
+    const foundFavSongs = favsList
+      .some((songs) => songs.trackId === trackId);
+    if (foundFavSongs) this.setState({ isFavorite: foundFavSongs });
+  };
+
   render() {
     const {
       track: {
         trackName,
         previewUrl,
+        trackId,
       },
     } = this.props;
-
+    const { loading, isFavorite } = this.state;
+    if (loading) {
+      return (
+        <Loading />
+      );
+    }
     return (
       <div>
         <section>
@@ -25,6 +72,17 @@ class MusicCard extends Component {
             </code>
             .
           </audio>
+          <label htmlFor="favorite">
+            Favorita
+            <input
+              name="favorite"
+              type="checkbox"
+              data-testid={ `checkbox-music-${trackId}` }
+              checked={ isFavorite }
+              id={ trackId }
+              onChange={ this.handleSave }
+            />
+          </label>
         </section>
       </div>
     );
@@ -36,6 +94,8 @@ MusicCard.propTypes = {
     trackName: string.isRequired,
     previewUrl: string.isRequired,
   }).isRequired,
+  musicInfo: string.isRequired,
+  trackId: string.isRequired,
 };
 
 export default MusicCard;
